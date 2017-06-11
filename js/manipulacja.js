e0c8f2a42e9bbc2997e909a3995cce31f1eea91b
Artur Grzes <grzesa@poczta.onet.pl>
2017-06-11 11:32:19 +0200

    2

--- js/manipulacja.js
+++ js/manipulacja.js
@@ -1,7 +1,18 @@
 'use script'
 
 var mainHeader = document.getElementById('main-header');
+//przypisujemy nagłówek strony do zmiennej
 
 console.log(mainHeader);
 
 mainHeader.innerHTML = "Treść nagłówka";
+// dodajemy treść do nagłówka
+
+var link = document.getElementsByClassName('link')[0]; //przypisujemi pierwszy link do zmiennej
+console.log(link);
+
+link.href = "http://akademia108.pl"; //nadpisujemy atrybut href w linku
+
+link.className = "nowa-klasa"; //nadpisujemy nazwę klasy w linku
+
+mainHeader.style.color = "#11aa22"; //dodajemy styl do nagłówka
