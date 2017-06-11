50aec305d554ce9ddb590234a39ec7268d2f6b10
Artur Grzes <grzesa@poczta.onet.pl>
2017-06-11 12:27:40 +0200

    4

--- js/zdarzeniaEventy.js
+++ js/zdarzeniaEventy.js
@@ -5,3 +5,13 @@ var secondLink = document.getElementsByTagName('a')[1];
 //tworzymy zmienną z drugim linkiem
 
 console.log(secondLink);
+
+
+function alarm(event) {
+   event.preventDefault(); //zapobiegamy domyślnej akcji
+    console.log('kliknięto kolejny link');
+    console.log(event);
+}
+    
+    
+    secondLink.onclick = alarm; //wywołujemy funkcję alarm na drugim linku po kliknięciu
